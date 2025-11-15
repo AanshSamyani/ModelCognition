32fcd1faf9195ff5f6879e61932b06fa8cc0682b
AanshSamyani <aanshsamyani123@gmail.com>
2025-11-15 21:40:17 +0530

    multi-token log probs

--- code/get_log_probs.py
+++ code/get_log_probs.py
@@ -48,6 +48,68 @@ def get_single_token_log_probs(
     with open(output_path, "w") as f:
         json.dump(data, f, indent=4)
         
+def get_multi_token_log_probs(
+    model: AutoModelForCausalLM, 
+    tokenizer: AutoTokenizer, 
+    dataset_path: str, 
+    output_dir: str,
+    output_filename: str,
+    is_user_prompt: bool = True,
+) -> None:
     
-    
+    with open(dataset_path, "r") as f:
+        data = json.load(f)
+        
+    for prompt in tqdm(data):
+        if is_user_prompt:
+            user_prompt = f"""
+            Complete the following sentence:
+            {prompt["prompt"]} 
+            """
+            message_1 = [
+                {"role": "user", "content": user_prompt}, 
+                {"role": "assistant", "content": prompt["completion_1"]},
+            ]
+            
+            message_2 = [
+                {"role": "user", "content": user_prompt},
+                {"role": "assistant", "content": prompt["completion_2"]}
+            ]
+        
+        else:
+            user_prompt = f"""
+            Write down a statement.
+            """
+            
+            message_1 = [
+                {"role": "user", "content": user_prompt}, 
+                {"role": "assistant", "content": prompt["completion_1"]},
+            ]
+            
+            message_2 = [
+                {"role": "user", "content": user_prompt},
+                {"role": "assistant", "content": prompt["completion_2"]}
+            ]
+            
+        inputs_temp_1 = tokenizer.apply_chat_template(message_1, tokenize=False)
+        inputs_1 = tokenizer(inputs_temp_1, return_tensors="pt").to(model.device)
+        
+        start_token_idx_1 = find_assistant_token_end(inputs_1["input_ids"][0].tolist())
+        end_token_idx_1 = len(inputs_1["input_ids"][0]) - 2
+        
+        outputs_1 = model(**inputs_1)
+        logits_1 = outputs_1.logits[0]
+        
+        log_probs_1 = 1
+        for token_idx in range(start_token_idx_1, end_token_idx_1): 
+            log_probs_1 = log_probs_1 * logits_1[token_idx, inputs_1["input_ids"][0][token_idx + 1]]
+            
+        inputs_temp_2 = tokenizer.apply_chat_template(message_2, tokenize=False)
+        inputs_2 = tokenizer(inputs_temp_2, return_tensors="pt".to(model.device))
+        
+        
+            
+            
+
+
     
