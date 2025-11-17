31f3c2588980ebab04faefda0db259c5c0a5d7e6
AanshSamyani <aanshsamyani123@gmail.com>
2025-11-18 04:46:22 +0530

    Updated multi-token results

--- code/experiment1.py
+++ code/experiment1.py
@@ -12,8 +12,8 @@ from evaluation import evaluate_model_on_multi_token_completion, evaluate_model_
 @dataclass
 class ExperimentConfig:
     model_name: str = "/nlsasfs/home/isea/isea10/aansh/deception_detection/weights/Llama-3.3-70B-Instruct/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b"
-    dataset_path: str = "/nlsasfs/home/isea/isea10/aansh/introspection/data/exp1/multi_token_completions/object_function_ambiguity_category_3.json"
-    output_dir: str = "/nlsasfs/home/isea/isea10/aansh/introspection/results/exp_1_llama_70b/multi_token/category_3"
+    dataset_path: str = "/nlsasfs/home/isea/isea10/aansh/introspection/data/exp1/multi_token_completions/descriptive_scene_additions_category_5.json"
+    output_dir: str = "/nlsasfs/home/isea/isea10/aansh/introspection/results/exp_1_llama_70b/multi_token/category_5"
     log_probs_output_filename: str = "log_probs.json"
     evaluation_output_filename: str = "predictions.json"
     metrics_output_filename: str = "metrics.json"
